34bb9d43995cc0fc8976e089dd2d6ba4f9ae6360
nithinRaj031197 <nithinraj.official3@gmail.com>
2023-07-17 01:53:18 +0530

    Leetcode: 872 Leaf Similar Trees

---
+++ Trees/Leetcode/872_Leaf-Similar Trees.py
@@ -0,0 +1,79 @@
+"""
+Consider all the leaves of a binary tree, from left to right order, the values of those leaves form a leaf value sequence.
+
+
+
+For example, in the given tree above, the leaf value sequence is (6, 7, 4, 9, 8).
+
+Two binary trees are considered leaf-similar if their leaf value sequence is the same.
+
+Return true if and only if the two given trees with head nodes root1 and root2 are leaf-similar.
+
+
+
+Example 1:
+
+
+Input: root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
+Output: true
+Example 2:
+
+
+Input: root1 = [1,2,3], root2 = [1,3,2]
+Output: false
+"""
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def solve(root1, root2):
+    def getLeaves(root):
+        leaves = []
+        stack = [root]
+
+        while stack:
+            curr_node = stack.pop()
+
+            if not curr_node.left and not curr_node.right:
+                leaves.append(curr_node.val)
+
+            if curr_node.left:
+                stack.append(curr_node.left)
+
+            if curr_node.right:
+                stack.append(curr_node.right)
+
+        return leaves
+
+    return getLeaves(root1) == getLeaves(root2)
+
+
+# Create root1
+root1 = TreeNode(3)
+root1.left = TreeNode(5)
+root1.right = TreeNode(1)
+root1.left.left = TreeNode(6)
+root1.left.right = TreeNode(2)
+root1.left.right.left = TreeNode(7)
+root1.left.right.right = TreeNode(4)
+root1.right.left = TreeNode(9)
+root1.right.right = TreeNode(8)
+
+# Create root2
+root2 = TreeNode(3)
+root2.left = TreeNode(5)
+root2.right = TreeNode(1)
+root2.left.left = TreeNode(6)
+root2.left.right = TreeNode(7)
+root2.left.right.left = None
+root2.left.right.right = None
+root2.right.left = TreeNode(4)
+root2.right.right = TreeNode(2)
+root2.right.right.left = TreeNode(9)
+root2.right.right.right = TreeNode(8)
+print(solve(root1, root2))
